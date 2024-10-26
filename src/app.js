c733b6018a32a20f12ee6dcbc9f7c772ec5699a9
Roderick <roypanatec@gmail.com>
2024-10-26 12:04:16 -0500

    new url integration

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ app.use(cors({
     origin: [
         'https://react-weeb-59a5zoags-roy2995s-projects.vercel.app/',
         'https://react-weeb-59a5zoags-roy2995s-projects.vercel.app',
-        'http://localhost:5173/'
+        'http://localhost:5173/',
+        'https://react-weeb-m2nf2ajke-roy2995s-projects.vercel.app/'
     ]
 }));
 
